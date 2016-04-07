80a86129a37673ffb137e139781b75a314b7e3ae
cristianpinto <cristianpintoquinto@gmail.com>
2016-04-07 17:32:25 +0200

    Initial Commit

---
+++ iOSMusicManageTests/iOSMusicManageTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOSMusicManageTests.swift
+//  iOSMusicManageTests
+//
+//  Created by Cristian Miguel Pinto Quinto on 07/04/16.
+//  Copyright © 2016 Cristian Pinto. All rights reserved.
+//
+
+import XCTest
+@testable import iOSMusicManage
+
+class iOSMusicManageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
