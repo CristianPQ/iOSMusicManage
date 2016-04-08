b5afe353d58161ad61cd33975da6c95815e67c51
cristianpinto <cristianpintoquinto@gmail.com>
2016-04-08 09:19:59 +0200

    Login defined with autolayout

---
+++ LoginViewController.swift
@@ -0,0 +1,49 @@
+//
+//  LoginViewController.swift
+//  iOSMusicManage
+//
+//  Created by Cristian Miguel Pinto Quinto on 08/04/16.
+//  Copyright © 2016 Cristian Pinto. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func loginTo(sender: UIButton) {
+        
+        //user correcto
+        //pass correcta
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //security passwordField
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOSMusicManage/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  iOSMusicManage
+//
+//  Created by Cristian Miguel Pinto Quinto on 08/04/16.
+//  Copyright © 2016 Cristian Pinto. All rights reserved.
+//
+
+import Cocoa
+
+class LoginViewController: UIViewController {
+
+}
